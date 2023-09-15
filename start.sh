69fdc332b97c81e9b0f05fbfa6130eec730edb86
Muammar Khadafi <daffigusti0890@gmail.com>
2023-09-15 07:09:37 +0700

    update

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@ docker network create kong-net
 docker-compose up --build -d db kong-migrations
 sleep 10
 docker-compose up --build -d kong konga
+
+https://computingforgeeks.com/run-elastic-stack-elk-on-docker/
